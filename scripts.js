e21ad0be2ef52b7d94562acb1b4ba03aa92c0fca
redsight <malahov.dmitry@gmail.com>
2020-06-17 23:55:40 +0400

    К отправке

--- scripts.js
+++ scripts.js
@@ -1,25 +1,77 @@
-let xmlDoc;
-
 window.onload = async function () {
+  const API_URL =
+    "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+  const corsHackURL = "https://cors-anywhere.herokuapp.com/"; // Обход no-cors на сервере, только для тестирования
+  const fetchURL = corsHackURL + API_URL;
+
+  // Получение данных с серевра и преобразоование в удобный вид
   const response = await fetch(fetchURL);
   const xmlString = await response.text();
   const parser = new DOMParser();
-  xmlDoc = parser.parseFromString(xmlString, "text/xml");
-  //   console.log(xmlDoc.querySelector("Cube[time]"));
-  renderTable(xmlDoc);
+  const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+  const ratesToEUR = xmlToObj(xmlDoc);
+
+  // Отображение элементов на странице
+  renderSelect(ratesToEUR);
+  renderTable(ratesToEUR);
+
+  //Привязываем обработчик событий к select'у
+  document
+    .getElementById("select-currency")
+    .addEventListener("change", (evt) => {
+      const value = evt.target.value;
+      let newRates = rateToCurrency(ratesToEUR, value);
+      renderTable(newRates);
+    });
 };
 
-function renderTable(xmlDoc) {
+const rounding = 4; //Округление курса до 4 знаков после запятой, как у ЦБ РФ
+
+//Преобразование полученных данных в массив объектов
+function xmlToObj(xmlDoc) {
   const Cubes = xmlDoc.querySelector("Cube[time]").children;
+  let rates = [];
+  for (let node of Cubes) {
+    rates.push({
+      rate: parseFloat(node.getAttribute("rate")).toFixed(rounding),
+      currency: node.getAttribute("currency"),
+    });
+  }
+  rates.push({
+    rate: "1",
+    currency: "EUR",
+  });
+  rates = rates.sort((a, b) => a.currency.localeCompare(b.currency));
+  return rates;
+}
+
+// Отображение опции для элемента select
+function renderSelect(rates) {
+  const reversedRates = [...rates].reverse();
+  let selectEl = document.getElementById("select-currency");
+  for (rate of reversedRates) {
+    selectEl.add(new Option(rate.currency, rate.rate));
+  }
+  selectEl.value = 1;
+}
+
+// Отображение таблицы
+function renderTable(rates) {
   let tableHTML = "";
-  for (node of Cubes) {
-    const rate = node.getAttribute("rate");
-    const currency = node.getAttribute("currency");
-    tableHTML += `<tr><td>${currency}</td><td>${rate}</td></tr>`;
+  for (rate of rates) {
+    tableHTML += `<tr><td>${rate.currency}</td><td>${rate.rate}</td></tr>`;
   }
   document.getElementById("currency-table-body").innerHTML = tableHTML;
 }
 
-const corsHackURL = "https://cors-anywhere.herokuapp.com/";
-const API_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
-const fetchURL = corsHackURL + API_URL;
+// Возвращение массива с курсами относительно выбранной валюты
+function rateToCurrency(ratesToEUR, value) {
+  // в параметрах передаем отношение валюты к евро, а не название
+  const newRates = ratesToEUR.map((rate) => {
+    return {
+      currency: rate.currency,
+      rate: (rate.rate / value).toFixed(rounding),
+    };
+  });
+  return newRates;
+}
